d22df3b1e84d5f465fd6d64efe931466531dbe92
Unknown <martin.goutry@gmail.com>
2018-04-08 19:43:56 +0200

    Add gulp task for deploy

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,10 +1,30 @@
 import gulp from 'gulp'
 import runSequence from 'run-sequence'
 
-const defaultTask = () => (
-  runSequence('copy:fonts', 'icons', 'images', 'styles', 'scripts:vendors', 'scripts:app', 'views', 'server')
-)
+const defaultTask = () =>
+  runSequence(
+    'copy:fonts',
+    'icons',
+    'images',
+    'styles',
+    'scripts:vendors',
+    'scripts:app',
+    'views',
+    'server'
+  )
+
+const productionTask = () =>
+  runSequence(
+    'copy:fonts',
+    'icons',
+    'images',
+    'styles',
+    'scripts:vendors',
+    'scripts:app',
+    'views'
+  )
 
 gulp.task('default', defaultTask)
+gulp.task('build', productionTask)
 
 export default defaultTask
